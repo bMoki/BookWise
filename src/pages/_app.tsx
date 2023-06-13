58ff7c6971d32af2d5120ad157d99176c0a711dd
bmoki <breno26sonda@gmail.com>
2023-06-13 14:20:02 -0300

    init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
+/* eslint-disable camelcase */
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Nunito_Sans } from 'next/font/google'
+
+const nunitoSans = Nunito_Sans({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-nunitoSans',
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={nunitoSans.variable}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
